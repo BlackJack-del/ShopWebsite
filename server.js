9668de907b126351aa810302981f5434a2a23c00
BlackJack-del <assassinxyz399@example.com>
2024-08-21 23:55:12 +0700

    ...

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,9 +9,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'statics')));
 
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
