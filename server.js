8c12fc680fd747b6b5ed102a1cbfb5d7c06c3242
BlackJack-del <assassinxyz399@example.com>
2024-08-21 15:59:12 +0700

    another update

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files (HTML, CSS, JS, Images)
-app.use(express.static('public'));
+// Serve static files (CSS, JS, Images)
+app.use(express.static(path.join(__dirname, 'statics')));
 
 // Handle the main route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
 // Start the server
