86f30ab0c87ee1cbb5930df956fcd2c9e8e26303
BlackJack-del <assassinxyz399@example.com>
2024-08-22 17:07:20 +0700

    edit

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'statics')));
 
 app.get('/', (req, res) => {
--- statics/scripts/home.js
+++ statics/scripts/home.js
@@ -1,17 +1,20 @@
-document.getElementById('menu-button').addEventListener('click', () => {
-    document.getElementById('sidebar').style.left = '0';
-    document.getElementById('overlay').style.opacity = '1';
-    document.getElementById('overlay').style.visibility = 'visible';
-});
+// /scripts/home.js
 
-document.getElementById('navbar-menu-button').addEventListener('click', () => {
-    document.getElementById('sidebar').style.left = '-250px';
-    document.getElementById('overlay').style.opacity = '0';
-    document.getElementById('overlay').style.visibility = 'hidden';
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu-button');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
 
-document.getElementById('overlay').addEventListener('click', () => {
-    document.getElementById('sidebar').style.left = '-250px';
-    document.getElementById('overlay').style.opacity = '0';
-    document.getElementById('overlay').style.visibility = 'hidden';
+    menuButton.addEventListener('click', () => {
+        const isSidebarVisible = sidebar.classList.contains('show');
+        sidebar.classList.toggle('show', !isSidebarVisible);
+        overlay.style.opacity = isSidebarVisible ? '0' : '1';
+        overlay.style.visibility = isSidebarVisible ? 'hidden' : 'visible';
+    });
+
+    overlay.addEventListener('click', () => {
+        sidebar.classList.remove('show');
+        overlay.style.opacity = '0';
+        overlay.style.visibility = 'hidden';
+    });
 });
