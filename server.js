6d83697014f18a43b1b6e32dd9fd72c931c0d112
BlackJack-del <assassinxyz399@example.com>
2024-08-24 23:48:46 +0700

    good topbar :)

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'statics')));
 
 app.get('/', (req, res) => {
