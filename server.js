763ecd97ebb5102c11e4154c04de9610e031a3d2
BlackJack-del <assassinxyz399@example.com>
2024-08-27 23:56:20 +0700

    more update

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
+// Set up EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Serve static files
 app.use(express.static(path.join(__dirname, 'statics')));
 
+// Route for home page
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+// Route for sign-up page
+app.get('/signup', (req, res) => {
+  res.render('signup'); // Render signup.ejs from views directory
 });
 
 app.listen(port, () => {
